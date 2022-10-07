f059196d65a557072c63b7c26cf552328d54a208
wjfatuan <wilson.forero@uan.edu.co>
2022-10-07 11:46:19 +1000

    Using Ion library and dynamic UI to display the cats

--- app/src/main/java/co/edu/uan/android/sample/ejemploapis105/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/sample/ejemploapis105/MainActivity.kt
@@ -3,8 +3,11 @@ package co.edu.uan.android.sample.ejemploapis105
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import co.edu.uan.android.sample.ejemploapis105.databinding.ActivityMainBinding
 import com.koushikdutta.async.future.FutureCallback
 import com.koushikdutta.ion.Ion
+import com.squareup.picasso.Picasso
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
@@ -12,23 +15,20 @@ import java.lang.Exception
 import java.util.concurrent.Future
 
 class MainActivity : AppCompatActivity() {
-    val jsonData = "{\n" +
-            "  \"private\": \"true\",              " +
-            "  \"from\": \"Alice (alice@ex.com)\", " +
-            "  \"subject\": \"Today's event\",     " +
-            "  \"to\": [                          " +
-            "    \"Robert (roberto@ex.com)\",     " +
-            "    \"Charles (cdodd@ex.com)\"       " +
-            "  ],\n" +
-            "  \"message\": {                     " +
-            "    \"lang\": \"english\",           " +
-            "    \"text\": \"Call this weekend!\" " +
-            "  }\n" +
-            "}\n"
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(this.layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnLoadCats.setOnClickListener {
+            loadCats()
+        }
+
+    }
+
+    fun loadCats() {
         Ion.with(this)
             .load("https://api.thecatapi.com/v1/images/search")
             .asString()
@@ -38,24 +38,33 @@ class MainActivity : AppCompatActivity() {
                 }
 
             })
-
     }
 
     fun processData(data: String) {
         try {
-            // [{"id":"b2f","url":"https://cdn2.thecatapi.com/images/b2f.jpg","width":500,"height":375}]
             // extract the information from JSON data
             val json = JSONArray(data)
             val cat = json.get(0) as JSONObject
-            val id = cat.getString("id")
             val url = cat.getString("url")
             val width = cat.getInt("width")
             val height = cat.getInt("height")
             Log.v("JSONDATA",json.toString())
+            addCatImage(url)
 
         } catch (e: JSONException) {
             Log.wtf("JSONDATA", e)
         }
+    }
+
+    fun addCatImage(url: String) {
+        val container = binding.catsContainer
+        val iv = ImageView(this)
+        // use picasso to load the image
+        Picasso
+            .get()
+            .load(url)
+            .into(iv)
+        container.addView(iv)
     }
 
 }
